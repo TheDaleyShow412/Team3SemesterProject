119d32ac560c705d7d90b1e567b3e5b1548fe762
Devin Daley <devindaley11@gmail.com>
2021-03-01 09:58:58 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team3SemesterProject.Startup))]
+namespace Team3SemesterProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
